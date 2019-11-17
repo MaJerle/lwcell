b0a51f394540357c15ace766a10d4ba3ef680b7a
Tasosgi <31473443+Tasosgi@users.noreply.github.com>
2019-11-17 21:32:34 +0200

    Update gsm_sys.h

--- gsm_at_lib/src/include/system/gsm_sys.h
+++ gsm_at_lib/src/include/system/gsm_sys.h
@@ -77,7 +77,7 @@ typedef void (*gsm_sys_thread_fn)(void *);
 #if GSM_CFG_SYS_PORT == GSM_SYS_PORT_CMSIS_OS
 #include "system/gsm_sys_cmsis_os.h"
 #elif GSM_CFG_SYS_PORT == GSM_SYS_PORT_CMSIS_OS2
-#include "system/esp_sys_cmsis_os2.h"
+#include "system/gsm_sys_cmsis_os2.h"
 #elif GSM_CFG_SYS_PORT == GSM_SYS_PORT_WIN32
 #include "system/gsm_sys_win32.h"
 #elif GSM_CFG_SYS_PORT == GSM_SYS_PORT_USER
