dd7a3beebc042ac905ae65ec08c7727c5a1cd4c9
Tilen Majerle <tilen.majerle@gmail.com>
2019-12-27 21:25:19 +0100

    Update copyright name

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ subprocess.call('doxygen doxy_gsm.doxy', shell=True)
 # -- Project information -----------------------------------------------------
 
 project = 'GSM-AT Lib'
-copyright = '2019, Tilen Majerle'
+copyright = '2019, Tilen MAJERLE'
 author = 'Tilen MAJERLE'
 
 # The full version, including alpha/beta/rc tags
