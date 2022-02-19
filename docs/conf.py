ff58df3fc25886d1e9491dc2e393afbf07d5bde7
Tilen Majerle <tilen.majerle@gmail.com>
2022-02-19 01:47:05 +0100

    Update requirements file for latest rtd theme

--- docs/conf.py
+++ docs/conf.py
@@ -121,15 +121,13 @@ html_js_files = [
     ''
 ]
 
+# Master index file
 master_doc = 'index'
 
-#
-# Breathe configuration
-#
-#
-#
+# --- Breathe configuration -----------------------------------------------------
 breathe_projects = {
 	"lwgsm": "_build/xml/"
 }
 breathe_default_project = "lwgsm"
 breathe_default_members = ('members', 'undoc-members')
+breathe_show_enumvalue_initializer = True
